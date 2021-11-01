e51411f6cef6f26ba89c26d07c2784fc00220f2a
Denis Nurdinov <dragon.slayer.dev@gmail.com>
2021-11-01 21:49:50 +0300

    Initial commit

---
+++ src/main/java/ru/dragon/develop/AuthServiceRun.java
@@ -0,0 +1,11 @@
+package ru.dragon.develop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceRun {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceRun.class, args);
+    }
+}
